fbf2d8106f75bea7a39c8609ae70a2fac7d7115e
ericmlantz <ericmlantz@gmail.com>
2024-08-21 11:43:23 -0400

    Updated getDataPoint()'s how price is computed in. Updated getRetio()'s return value to the ratio of stock price_a to stock price_b as well as created check in case price_b is 0. Updated main() with prices dictionary to store the stock prices.

--- client3.py
+++ client3.py
@@ -30,19 +30,20 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # Updated how price is computed
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if (price_b == 0):
+        # In cases where price_b is 0, this avoids a ZeroDivisionError
+        return
+    # Updated return value to the raito of stock price_a to stock price_b
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +52,11 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Created price dictionary to store stock prices
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
